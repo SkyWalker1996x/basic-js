c5929596733e0fd0d55ecf33b18b92e24a2ea056
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-11-01 19:37:44 +0200

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,48 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(stat = true) {
+    this.stat = stat;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error();
+    const alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    message = message.toUpperCase().split("");
+    key = key.toUpperCase().split("");
+    let i = 0;
+    let j = 0;
+
+    while (i < message.length) {
+      if (alp.indexOf(message[i]) !== -1) {
+        message[i] =
+          alp[(alp.indexOf(message[i]) + alp.indexOf(key[j])) % alp.length];
+        j = ++j % key.length;
+      }
+      i++;
+    }
+    return this.stat ? message.join("") : message.reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error();
+    const alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    encryptedMessage = encryptedMessage.toUpperCase().split("");
+    key = key.toUpperCase().split("");
+    let i = 0;
+    let j = 0;
+    while (i < encryptedMessage.length) {
+      if (alp.indexOf(encryptedMessage[i]) !== -1) {
+        encryptedMessage[i] =
+          alp[
+            (alp.indexOf(encryptedMessage[i]) +
+              alp.length -
+              alp.indexOf(key[j])) %
+              alp.length
+          ];
+        j = ++j % key.length;
+      }
+      i++;
+    }
+    return this.stat
+      ? encryptedMessage.join("")
+      : encryptedMessage.reverse().join("");
   }
 }
 
