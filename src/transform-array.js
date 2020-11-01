63549c4912f7ab87155bbb282e44ec036e3346af
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-11-01 16:48:18 +0200

    feat: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+const transform = (arr) => {
+  let copyArr = [...arr];
+  let newArr = [];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  for (let i = 0; i < copyArr.length; i++) {
+    if (copyArr[i] === "--discard-next") {
+      copyArr[i + 1] = "err";
+      i++;
+    } else if (copyArr[i] === "--discard-prev") {
+      if (newArr.length !== 0 && copyArr[i - 1] !== "err") newArr.pop();
+    } else if (copyArr[i] === "--double-next") {
+      if (i + 1 < copyArr.length) newArr.push(copyArr[i + 1]);
+    } else if (copyArr[i] === "--double-prev") {
+      if (newArr.length !== 0 && copyArr[i - 1] !== "err")
+        newArr.push(newArr[newArr.length - 1]);
+    } else {
+      newArr.push(copyArr[i]);
+    }
+  }
+  return newArr;
 };
+
+module.exports = transform;
